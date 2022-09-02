0e16886c5cb155ec38ef01898cc5cfd8bc5a9181
Max Kottong <max.kottong@gmail.com>
2022-09-02 14:07:14 -0400

    Finished adding all initial files

---
+++ models/User.js
@@ -0,0 +1,14 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+
+
+}
+
+User.init({
+
+});
+
+module.exports = User;
