206978c849f655225525a2ff7eb9bf0bea555b4d
Max Kottong <max.kottong@gmail.com>
2022-09-04 13:45:25 -0400

    Added home-routes

--- controllers/index.js
+++ controllers/index.js
@@ -5,7 +5,7 @@ const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./dashboard-routes');
 
 router.use('/dashboard', dashboardRoutes);
-/*router.use('/', homeRoutes);*/
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
