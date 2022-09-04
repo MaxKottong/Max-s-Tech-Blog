68f8f1887fb44a9efcdb9096a1094859814b5c2c
Max Kottong <max.kottong@gmail.com>
2022-09-04 13:44:38 -0400

    Added dashboard routes

--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./dashboard-routes');
 
-/*router.use('/dashboard', dashboardRoutes);*/
+router.use('/dashboard', dashboardRoutes);
 /*router.use('/', homeRoutes);*/
 router.use('/api', apiRoutes);
 
